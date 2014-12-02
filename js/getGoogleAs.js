f9b6f283cfca4546942f5a65f40b06afd86195ee
vmovier <service@vmovier.com>
2014-12-02 18:18:25 +0800

    除去重定向

--- js/getGoogleAs.js
+++ js/getGoogleAs.js
@@ -1,5 +1,13 @@
 var searchs = document.getElementsByClassName('r');
 
 for (var i = 0; i < searchs.length; ++i) {
+	var _href = searchs[i].childNodes[0].getAttribute('href'),
+		_text = searchs[i].childNodes[0].innerText,
+		_text = document.createTextNode(_text);
+console.log(searchs[i].childNodes[0]);
+	// searchs[i].childNodes[0].removeAttribute('href');
 	searchs[i].childNodes[0].removeAttribute('onmousedown');
+	searchs[i].childNodes[0].removeAttribute('onclick');
+	searchs[i].childNodes[0].removeAttribute('target');
+	searchs[i].childNodes[0].removeAttribute('data-href');
 }
